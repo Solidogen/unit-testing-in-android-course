638ef9674f47b7dfccc80b31e706150efd8c7d26
patrycjusz.blaszczuk <patrycjusz.blaszczuk@gmail.com>
2019-07-29 22:03:38 +0200

    1.3

--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidatorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidatorTest.java
@@ -1,5 +1,32 @@
 package com.techyourchance.unittestingfundamentals.exercise1;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 public class NegativeNumberValidatorTest {
 
+    private NegativeNumberValidator SUT;
+
+    @Before
+    public void setup() {
+        SUT = new NegativeNumberValidator();
+    }
+
+    @Test
+    public void testNegativeNumber() {
+        assertThat(SUT.isNegative(-1), is(true));
+    }
+
+    @Test
+    public void testZero() {
+        assertThat(SUT.isNegative(0), is(false));
+    }
+
+    @Test
+    public void testPositiveNumber() {
+        assertThat(SUT.isNegative(1), is(false));
+    }
 }
